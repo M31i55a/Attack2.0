7d27700d98ab0fe42254fe06648ea67c43959def
M31i55a <melysgioda34@gmail.com>
2023-06-29 09:09:26 +0300

    basic debug mode

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ window.addEventListener('load', function(){
                 else if(e.key === ' '){
                     this.game.player.shootTop();
                 }
+                else if(e.key === 'd'){
+                    this.game.debug = !this.game.debug;
+                }
             })
             window.addEventListener('keyup', (e) => {
                 if(this.game.keys.indexOf(e.key) > -1){
@@ -89,8 +92,7 @@ window.addEventListener('load', function(){
 
         draw(context){
             //player basic style(a black square)
-            context.fillStyle = 'black'
-            context.fillRect(this.x, this.y, this.width, this.height);
+            if(this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
             context.drawImage(this.image,this.frameX * this.width, this.frameY * this.height, this.width, this.height,  this.x, this.y, this.width, this.height);
             this.projectiles.forEach(projectile => {
                 projectile.draw(context);
@@ -252,6 +254,7 @@ window.addEventListener('load', function(){
             this.gameTime = 0;
             this.timeLimit = 5000;
             this.speed = 1;
+            this.debug = true;
         }
 
         update(deltaTime){
