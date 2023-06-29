9bb35555035051602bd54bb7d0c2f870fc5078dc
M31i55a <melysgioda34@gmail.com>
2023-06-29 09:48:04 +0300

    first enemy type added

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ window.addEventListener('load', function(){
         }
 
         update(){
-            this.x += this.speed;
+            this.x += this.speed - this.game.speed;
             if(this.x > this.game.width * 0.8) this.markedForDeletion = true;
         }
 
@@ -115,17 +115,27 @@ window.addEventListener('load', function(){
             this.markedForDeletion = false;
             this.lives = 5;
             this.score = this.lives;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.maxFrame = 37;
         }
 
         update(){
             this.x += this.speedX;
             if(this.x + this.width < 0) this.markedForDeletion = true;
+
+            //sprite animation
+            if(this.frameX < this.maxFrame){
+                this.frameX++;
+            }
+            else{
+                this.frameX = 0;
+            }
         }
 
         draw(context){
-            context.fillStyle = 'red';
-            context.fillRect(this.x, this.y, this.width, this.height);
-            //Draw enemies lives to check their lives statuses
+            if(this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             context.fillStyle = 'black';
             context.font = '20px Helvetica';
             context.fillText(this.lives, this.x, this.y);
@@ -135,9 +145,11 @@ window.addEventListener('load', function(){
     class Angler1 extends Enemy{
         constructor(game){
             super(game);
-            this.width = 228 * 0.3;
-            this.height = 169 * 0.3;
+            this.width = 228;
+            this.height = 169;
             this.y = Math.random() * (this.game.height * 0.9 - this.height);
+            this.image = document.getElementById('angler1');
+            this.frameY = Math.floor(Math.random() * 3);
         }
     }
 
