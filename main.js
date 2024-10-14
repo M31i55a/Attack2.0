9c5b09a136e7ad0725db6f55b5ccd530309b0225
M31i55a <melysgioda34@gmail.com>
2024-10-14 15:42:47 +0100

    added game icons

--- main.js
+++ main.js
@@ -492,6 +492,7 @@ window.addEventListener('load', function(){
             this.fontSize = 21;
             this.fontFamily = 'bangers';
             this.color = 'white';
+            
         }
         draw(context){
             context.save();
@@ -500,17 +501,39 @@ window.addEventListener('load', function(){
             context.shadowOffsetY = 2;
             context.shadowColor = 'black';
             context.font = this.fontSize + 'px ' + this.fontFamily;
+            
             //score 
-            context.fillText('Score : ' + this.game.score, 20, 40);
+            const score = new Image();
+            score.src = "assets/score.png"
+            context.drawImage(score, 10, 10, 37, 37);
+            context.fillText(' : '+ this.game.score, 50, 40);
+            
             //lives
-            context.fillText('Lives : ' + this.game.player.lives, 140, 40);
+            const lives = new Image();
+            lives.src = "assets/heart.png"
+            context.drawImage(lives, 120, 10, 37, 37);
+            context.fillText(' : ' + this.game.player.lives, 160, 40);
+            
             //display protect status
-            context.fillText('Protections : ' + this.game.player.backUpCounter, 260, 40)
+            const defense = new Image();
+            defense.src = "assets/defense.png"
+            context.drawImage(defense, 230, 10, 37, 37);
+            context.fillText(' : ' + this.game.player.backUpCounter, 270, 40)
+            
             //Timer
             const formattedTime = (this.game.gameTime * 0.001).toFixed(1);//Scientific Notation
-            context.fillText('Timer : ' + formattedTime, 20, 100);
-            //display coins
-            context.fillText('Coins : ' + this.game.player.dragonTrigger, 150, 100)
+            
+            const timer = new Image();
+            timer.src = "assets/timer.png"
+            context.drawImage(timer, 10, 75, 37, 37);
+            context.fillText(' : ' + formattedTime, 50, 100);
+            
+            //display coins(dragon)
+            const dragon = new Image();
+            dragon.src = "assets/dragon.png"
+            context.drawImage(dragon, 120, 75, 37, 37);
+            context.fillText(' : ' + this.game.player.dragonTrigger, 160, 100)
+            
             //Game Over Messages
             if(this.game.gameOver){
                 context.textAlign = 'center';
